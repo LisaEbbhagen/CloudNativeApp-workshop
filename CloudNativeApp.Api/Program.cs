0700b68908885f743a3c6d394cf4db07b635498c
Lisa Ebbhagen <lisa.ebbhagen@gmail.com>
2026-05-20 10:07:44 +0200

    added keyvault

--- CloudNativeApp.Api/Program.cs
+++ CloudNativeApp.Api/Program.cs
@@ -1,8 +1,19 @@
+using Azure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+var keyVaultUrl = builder.Configuration["KeyVaultUrl"];
+
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri(keyVaultUrl), 
+        new DefaultAzureCredential());
+}
 
 var app = builder.Build();
 
